35887a03ebed2115e11b109d9f537190d4763fb7
Henry <HWolf.binz@gmail.com>
2026-04-12 16:20:21 +0200

    feat(core): add Task class

---
+++ src/core/Task.ts
@@ -0,0 +1,31 @@
+import { Temporal } from 'temporal-polyfill';
+import type { ITask, PriorityLevel } from '../types/_task';
+
+export class Task implements ITask {
+  id: string = crypto.randomUUID();
+  title: string;
+  description?: string | undefined;
+  dueDate: Temporal.PlainDate = Temporal.Now.plainDateTimeISO().toPlainDate();
+  priority: PriorityLevel = 4;
+  isDone: boolean = false;
+
+  constructor(title: string) {
+    this.title = title;
+  }
+
+  updateTitle(newTitle: string): void {
+    this.title = newTitle;
+  }
+
+  updateDescription(newDescr: string): void {
+    this.description = newDescr;
+  }
+
+  toggleIsDone(): void {
+    this.isDone = !this.isDone;
+  }
+
+  setPriority(newPrio: PriorityLevel): void {
+    this.priority = newPrio;
+  }
+}
