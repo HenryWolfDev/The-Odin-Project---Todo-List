2f20e335e27db6b9e99e86b73b1091972e12c414
Henry <HWolf.binz@gmail.com>
2026-04-12 16:59:28 +0200

    feat(core): add methods in Project class

--- src/core/Project.ts
+++ src/core/Project.ts
@@ -9,4 +9,23 @@ export class Project implements IProject {
   constructor(name: string) {
     this.name = name;
   }
+
+  addTodo(task: Task): void {
+    this.todos.push(task);
+  }
+
+  removeTodo(id: string): void {
+    const taskIndex = this.todos.findIndex(task => task.id === id);
+    if (taskIndex !== -1) {
+      this.todos.splice(taskIndex, 1);
+    }
+  }
+
+  get totalCount(): number {
+    return this.todos.length;
+  }
+
+  get completedCount(): number {
+    return this.todos.filter(tasks => tasks.isDone).length;
+  }
 }
