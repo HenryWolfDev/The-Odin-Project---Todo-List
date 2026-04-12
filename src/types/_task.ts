120a01433915fa5fa85ed5f41d44e7f7fa83b978
Henry <HWolf.binz@gmail.com>
2026-04-12 16:09:48 +0200

    feat(interface): add Task interface

---
+++ src/types/_task.ts
@@ -0,0 +1,9 @@
+import { Temporal } from 'temporal-polyfill';
+export type PriorityLevel = 1 | 2 | 3 | 4;
+
+export interface ITask {
+  id: string;
+  title: string;
+  description?: string | undefined;
+  dueDate: Temporal.PlainDate | undefined;
+}
